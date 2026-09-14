02ec0862a25d64c2e0e9403d719764171b050d39
cnobert <cnobert@nait.ca>
2026-09-14 11:43:38 -0600

    sep 14 end of class

--- 03-intro-blog-example-continued-start/myblogwebsite/blog/views.py
+++ 03-intro-blog-example-continued-start/myblogwebsite/blog/views.py
@@ -1,3 +1,8 @@
+
 from django.shortcuts import render
 
-# Create your views here.
+
+# this is what will handle the request
+def post_list(request):
+    # note the following will render the template created.
+    return render(request, 'blog/posts_list.html')
